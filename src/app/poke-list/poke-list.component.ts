170f5b724f5e2e7ed8148cdb4c0db737bb17298f
Viktor <ricchiuto.viktor@gmail.com>
2021-05-22 10:10:01 +0200

    add poke card component

--- src/app/poke-list/poke-list.component.ts
+++ src/app/poke-list/poke-list.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+const pokemons = [
+  {id: 1, name: 'asd'},
+  {id: 2, name: 'sdfgd'},
+  {id: 3, name: '345g34'},
+  {id: 4, name: 'fntyuy'},
+]
+
 @Component({
   selector: 'app-poke-list',
   templateUrl: './poke-list.component.html',
