a329a6b820de4c856d8f88d94080b0a636fdc618
Viktor <ricchiuto.viktor@gmail.com>
2021-05-22 10:08:34 +0200

    add poke list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PokeDetailsComponent } from './poke-details/poke-details.component';
+import { PokeListComponent } from './poke-list/poke-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    PokeDetailsComponent
+    PokeDetailsComponent,
+    PokeListComponent
   ],
   imports: [
     BrowserModule,
